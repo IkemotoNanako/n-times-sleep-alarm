c1e2f8900fbec56cb2250e150b4ce3154068b881
IkemotoNanako <chibencaizi@gmail.com>
2023-05-08 22:18:33 +0900

    Initial Commit

---
+++ n times sleep alarm/n_times_sleep_alarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  n_times_sleep_alarmApp.swift
+//  n times sleep alarm
+//
+//  Created by n.ikemoto on 2023/05/08.
+//
+
+import SwiftUI
+
+@main
+struct n_times_sleep_alarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
